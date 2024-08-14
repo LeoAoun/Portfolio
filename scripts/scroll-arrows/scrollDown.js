11f23a293264f0cb7a9694663ef58f33e3f66e86
Leo Aoun <leonardoaoundev@gmail.com>
2024-08-14 15:23:48 -0300

    Scroll-down

---
+++ scripts/scroll-arrows/scrollDown.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollDown = document.getElementById("scroll-down");
+  setTimeout(() => {
+    scrollDown.style.display = "block";
+  }, 6000);
+});
